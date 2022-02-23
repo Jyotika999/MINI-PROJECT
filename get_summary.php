a558412075efa85303464a6348c897f6e411c145
Jyotika999 <Jyotika9september@gmail.com>
2022-02-23 20:34:34 +0530

    Created get_summary webpage

---
+++ get_summary.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- head starts  -->
+    <?php include 'includes/head.php' ?>
+    <!-- head ends  -->
+</head>
+<body>
+
+    <?php include 'includes/navbar.php' ?>
+
+	
+	
+	<div class="container">
+		<div class="row justify-content-center text-center">
+			<div class="col-10 py-5">
+				<h2>ABOUT </h2>
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+               
+			</div>
+		</div>
+	</div>
+	
+
+    <!-- footer starts -->
+	<?php include 'includes/footer.php' ?>
+    <!-- footer ends -->
+
+    <!--- Script Source Files -->
+        <? php include 'includes/scripts.php' ?>
+    <!--- End of Script Source Files -->
+
+</body>
+</html>
+
