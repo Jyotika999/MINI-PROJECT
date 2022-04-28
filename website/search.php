db6eabe9e536d947ef16789f901899d9fc55834f
shee35 <iit2019032@iiita.ac.in>
2022-04-29 00:13:28 +0530

    added navbar

--- website/search.php
+++ website/search.php
@@ -5,7 +5,7 @@
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light  fixed-top">
 		<a class="navbar-brand" href="index.php">Edu-Sum</a>
-		<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -13,12 +13,12 @@
 				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="query">
 				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 			</form>
-		</div> -->
+		</div>
 	</nav>
 	<br><br><br><br>
 	<div class="container">
 		<h4>Search Results</h4><br>
-		<!-- <?php
+		<?php
 			$query = $_GET["keyphrase"];
 			$dir = new DirectoryIterator("Videos/0001/Keywords_Splits/");
 			$countx = 0;
@@ -59,7 +59,7 @@
 					}
 				}
 			}
-			?> -->
+			?>
 	</div>
 </body>
 </html>
--- website/summary.php
+++ website/summary.php
@@ -5,7 +5,7 @@
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 		<a class="navbar-brand" href="index.php">Edu-sum</a>
-		<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -13,7 +13,7 @@
 				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="query">
 				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 			</form>
-		</div> -->
+		</div>
 	</nav>
 	<br><br><br>
 	<div class="container">
